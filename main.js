14ea18b6b9f8e5ad815658fad9d0ffdec609fdbf
Vikrant Rao <raovikrant82@gmail.com>
2021-07-01 15:33:16 +0500

    Edited the final draft

--- main.js
+++ main.js
@@ -8,13 +8,9 @@ function increment(){
     countEl.textContent = count;
 }
 
-increment()
- function save(){
+function save(){
     let countStr = count + " - ";
     saveEl.textContent += countStr;
     countEl.textContent = 0;
     count= 0;
 }
-
-save();
-
